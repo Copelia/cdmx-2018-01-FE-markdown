97fe0f0b86f647206b73bb254d0747dbe6badbb4
[Copelia] <iamcorallium@gmail.com>
2018-09-14 12:56:37 -0500

    Pasando data a segunda función con promsea}

--- index.js
+++ index.js
@@ -1,82 +1,73 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
-// const path = require('path');
-// './README.md'
-// let path = './README.md';
-
-// const file = ('./README.md');
-
-// const resolvePath = () => {
-//   let file = path.resolve();
-//   return file;
-// };
-
-// resolvePath();
-
-// Create reference instance
 const marked = require('marked');
 const cheerio = require('cheerio');
+let file = './README.md';
 
-
-// Resolver ruta parcial a absoluta
-// if (path.isAbsolute() === true) {
-const mdLinks = (callback) => {
-  fs.readFile('./README.md', 'utf8', (err, data) =>{
-    if (err) {
-      console.log('Error');
-    } else {
-      callback(data); 
+const mdLinks = new Promise((resolve, reject) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+	  if (err) {
+      reject(err);
     }
+    resolve(data);
   });
-};
-// } else {
-//   resolve(path);
-// }
-mdLinks(callback = (data) => {
-  // Convirtiendo markdown a html para sacar links
-  let htmlContent = marked(data);
-  // console.log(htmlContent); 
-  const $ = cheerio.load(htmlContent);
- 
-  const linksList = $('a').map(function(i, element) {
-    const hrefs = $(this).attr('href');
-    const anchorText = $(this).text();
-    //  Obteniendo attributo href para sacar status y texto como requisito
-
-    linkObject = {
-      href: hrefs,
-      text: anchorText,
-      file: './README.md'
-    };
-
-    return linkObject;
-  }).toArray();
-
-  // Declarando variable de arreglo de objetos para iterar hrefs
-  let links = linksList;
-  console.log(links);
-
-  const arrLinks = [];
-  for (let i = 0; i < links.length; i++) {
-    arrLinks.push(links[i].href);
-  }
-  // Obteniendo arreglo de links para probar promise.all
-  // console.log(arrLinks);
-
-  // map every url to the promise fetch
-  let requests = arrLinks.map(url => fetch(url));
-  // Promise.all waits until all jobs are resolved
-  Promise.all(requests)
-    .then(responses => responses.forEach(
-      // let result = await promise;
-      response => console.log(`${response.url}: ${response.status}`)
-    ))
-    .catch(()=>{
-      console.log('Error');
-    });
 });
 
 
-module.exports = {
-  mdLinks,
-};
+
+const infoLinks = (response) => {
+  console.log(response);
+  
+  // const htmlInfo = cheerio.load(fileToHtml);
+  // console.log(fileToHtml);
+  new Promise((resolve, reject) => {
+    if (false) {
+      reject('No existe archivo');
+    } else {
+      const fileToHtml = marked(response);
+      resolve();  
+    };
+  });    
+};      
+        
+ 
+//   const linksList = htmlInfo('a').map((val, i, element) => {
+//   const hrefs = htmlInfo(this).attr('href');
+//   const hrefText = htmlInfo(this).text();
+
+//   linkObject = {
+//     href: hrefs,
+//     text: hrefText,
+//     file: './README.md'
+//   };
+
+//   return linkObject;
+// }).toArray();
+
+//     console.log(linksList);
+//     resolve(linksList);
+//     });
+//   });
+// };
+	
+mdLinks
+  .then(
+    response => {
+      // console.log(response);
+
+      infoLinks(response);
+      // return infoLinks;
+    }
+  )
+  .then(
+    response2 => {
+      // console.log(response2);      
+    }
+  )
+  .catch(
+    err => console.log('Hubo un error:' + err.message));
+
+
+// module.exports = {
+//   mdLinks,
+// };
